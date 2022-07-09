6d1615d3ec2cf9bc1bba6cd33867adeb5138fdf5
kyshek904@gmail.com <kyshek904@gmail.com>
2022-07-10 00:00:47 +0800

    update

--- alb.tf
+++ alb.tf
@@ -31,7 +31,7 @@ resource "aws_lb_target_group" "app-lb-tg" {
 
 resource "aws_lb_listener" "jenkins-listener-http" {
     provider = aws.region-master
-    load_balancer_arn = aws_lb.application-lb.arn
+    load_balancer_arn = aws_alb.application-lb.arn
     port = "80"
     protocol = "HTTP"
     default_action {
