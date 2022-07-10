3c2b6c9670749c9a1ec00d98f62baad8f0d4fbce
kyshek904@gmail.com <kyshek904@gmail.com>
2022-07-10 17:53:50 +0800

    update

--- output.tf
+++ output.tf
@@ -1,20 +1,34 @@
-output "Jenkins-main-node-public-ip" {
-  value = aws_instance.jenkins-master.public_ip
+output "amiId-us-east-1" {
+  value = data.aws_ssm_parameter.linuxAmi.value
 }
 
-output "Jenkins-worker-public-IPs" {
+output "amiId-us-west-2" {
+  value = data.aws_ssm_parameter.linuxAmiOregon.value
+}
+output "Jenkins-Main-Node-Public-IP" {
+  value = aws_instance.jenkins-master.public_ip
+}
+output "Jenkins-Main-Node-Private-IP" {
+  value = aws_instance.jenkins-master.private_ip
+}
+output "Jenkins-Worker-Public-IPs" {
   value = {
-    for instance in aws_instance.jenkins-worker-uswest2 :
+    for instance in aws_instance.jenkins-worker-oregon :
     instance.id => instance.public_ip
   }
+}
+output "Jenkins-Worker-Private-IPs" {
+  value = {
+    for instance in aws_instance.jenkins-worker-oregon :
+    instance.id => instance.private_ip
+  }
+}
 
+output "url" {
+  value = aws_route53_record.jenkins.fqdn
 }
 
 output "LB-DNS-NAME" {
   value = aws_lb.application-lb.dns_name
   
-}
-
-output "LB-DNS-NAME" {
-  value = aws_lb.application-lb.dns_name
 }
