06b3c27215e8e4091f53fdf76072086b22578b16
kyshek904@gmail.com <kyshek904@gmail.com>
2022-07-10 18:13:08 +0800

    update

--- acm.tf
+++ acm.tf
@@ -5,7 +5,7 @@ resource "aws_acm_certificate" "jenkins-lb-https" {
     domain_name = join(".", ["jenkins", data.aws_route53_zone.dns.name])
     validation_method = "DNS"
     tags = {
-      "Name" = "Jenkins-ACM"
+      Name = "Jenkins-ACM"
     }
 }
 
