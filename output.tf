5ddf9987f9590625f08a4e23d2922c40936f6acd
kyshek904@gmail.com <kyshek904@gmail.com>
2022-07-10 17:57:23 +0800

    update

--- output.tf
+++ output.tf
@@ -1,10 +1,11 @@
-output "amiId-us-east-1" {
-  value = data.aws_ssm_parameter.linuxAmi.value
-}
+#output "amiId-us-east-1" {
+#  value = data.aws_ssm_parameter.linuxAmi.value
+#}
 
-output "amiId-us-west-2" {
-  value = data.aws_ssm_parameter.linuxAmiOregon.value
-}
+#output "amiId-us-west-2" {
+#  value = data.aws_ssm_parameter.linuxAmiOregon.value
+#}
+
 output "Jenkins-Main-Node-Public-IP" {
   value = aws_instance.jenkins-master.public_ip
 }
